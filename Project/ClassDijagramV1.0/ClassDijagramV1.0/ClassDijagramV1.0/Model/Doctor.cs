8e063b3d277d6d79362e3f984b80b533ffc77fb0
Sandra Brkic <sandra.brkic5@gmail.com>
2022-04-07 10:19:57 +0200

    Show data for patient examinations

--- Project/ClassDijagramV1.0/ClassDijagramV1.0/ClassDijagramV1.0/Model/Doctor.cs
+++ Project/ClassDijagramV1.0/ClassDijagramV1.0/ClassDijagramV1.0/Model/Doctor.cs
@@ -1,18 +1,76 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 
 namespace Model
 {
-   public class Doctor
-   {
-      private String id { get; set; }
-      private String name { get; set; }
+   public class Doctor : INotifyPropertyChanged
+    {
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged(string name)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(name));
+            }
+        }
+        private String id { get; set; }
+        private String name;
       private String surname { get; set; }
       private DateTime doB { get; set; }
       private DoctorType type { get; set; }
 
       //public Examination[] examination;
       private List<Examination> examination;
+
+        public String Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                if (value != name)
+                {
+                    name = value;
+                    OnPropertyChanged("Name");
+                }
+            }
+        }
+
+        public String Surname
+        {
+            get
+            {
+                return surname;
+            }
+            set
+            {
+                if (value != surname)
+                {
+                    surname = value;
+                    OnPropertyChanged("Surame");
+                }
+            }
+        }
+
+        public DoctorType Type
+        {
+            get
+            {
+                return type;
+            }
+            set
+            {
+                if (value != type)
+                {
+                    type = value;
+                    OnPropertyChanged("Type");
+                }
+            }
+        }
 
         public Doctor(string id, string name, string surname, DateTime doB, DoctorType type, List<Examination> examination)
         {
--- Project/ClassDijagramV1.0/ClassDijagramV1.0/ClassDijagramV1.0/Model/Room.cs
+++ Project/ClassDijagramV1.0/ClassDijagramV1.0/ClassDijagramV1.0/Model/Room.cs
@@ -1,16 +1,42 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 
 namespace Model
 {
-   public class Room
-   {
-      private String id { get; set; }
+   public class Room : INotifyPropertyChanged
+    {
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged(string name)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(name));
+            }
+        }
+        private String id { get; set; }
       private List<Equipment> equipment { get; set; }
       private int floor { get; set; }
       private int roomNb { get; set; }
       private bool occupancy { get; set; }
       private String type { get; set; }
+
+        public String Id
+        {
+            get
+            {
+                return id;
+            }
+            set
+            {
+                if (value != id)
+                {
+                    id = value;
+                    OnPropertyChanged("Id");
+                }
+            }
+        }
 
         public Room(string id, List<Equipment> equipment, int floor, int roomNb, bool occupancy, string type)
         {
