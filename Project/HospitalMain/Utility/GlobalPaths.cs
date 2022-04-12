6f11b6c9e94053a4acee898b7131d527fc2c139e
Vladimir Lunić <vladimir.lunic2000@gmail.com>
2022-04-12 22:23:43 +0200

    Dont delete .sln dir :)

---
+++ Project/HospitalMain/Utility/GlobalPaths.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace HospitalMain.Utility
+{
+    internal class GlobalPaths
+    {
+        public static String DBPath = Path.Combine(Environment.CurrentDirectory, @"..\..\..\..\HospitalMain\Database");
+        public static String RoomsDBPath = Path.Combine(DBPath, "Rooms.json");
+        public static String ExamsDBPath = Path.Combine(DBPath, "Exams.json");
+        public static String PatientsDBPath = Path.Combine(DBPath, "Patients.json");
+        public static String DoctorsDBPath = Path.Combine(DBPath, "Doctors.json");
+    }
+}
