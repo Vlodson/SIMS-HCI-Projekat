d7e3369ba55937b432266e1f27dbcd29edaf4bc6
Vladimir Lunić <vladimir.lunic2000@gmail.com>
2022-04-17 19:06:11 +0200

    Unify equipment and room windows to wireframe window

---
+++ Project/Admin/View/TableWindow.xaml.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.IO;
+
+using Model;
+using Utility;
+using Controller;
+using HospitalMain.Enums;
+
+namespace Admin.View
+{
+    /// <summary>
+    /// Interaction logic for EquipmentTableWindow.xaml
+    /// </summary>
+    public partial class TableWindow : Window
+    {
+
+        private EquipmentController _equipmentController;
+        private RoomController _roomController;
+
+        // the name will likely change to tableview and for each button press it will do (1)
+        public ObservableCollection<Equipment> EquipmentPerRoomList { get; set; }
+        public ObservableCollection<Room> RoomsList { get; set; }
+
+        public TableWindow()
+        {
+            InitializeComponent();
+            this.DataContext = this;
+
+            var app = Application.Current as App;
+            _equipmentController = app.equipmentController;
+            _roomController = app.roomController;
+
+            if (File.Exists(GlobalPaths.EquipmentDBPath))
+                _equipmentController.LoadEquipment();
+
+            if(File.Exists(GlobalPaths.RoomsDBPath))
+                _roomController.LoadRoom();
+
+            // this is (1), since it doesnt do crud stuff on it directly just change reference on view button row press
+            // and add new or something like that
+            EquipmentPerRoomList = _equipmentController.ReadAll();
+            RoomsList = _roomController.ReadAll();
+            
+        }
+
+        private void equipmentBtn_Click(object sender, RoutedEventArgs e)
+        {
+            TableGrid.Columns.Clear();
+
+            // create data grid programmatically
+            TableGrid.ItemsSource = EquipmentPerRoomList;
+
+            DataGridTextColumn id_col = new DataGridTextColumn()
+            {
+                Header = "ID",
+                Width = 100,
+                Binding = new Binding("Id")
+            };
+
+
+            DataGridTextColumn room_id_col = new DataGridTextColumn()
+            {
+                Header = "Room ID",
+                Width = 100,
+                Binding = new Binding("RoomId")
+            };
+
+            DataGridTextColumn type_col = new DataGridTextColumn()
+            {
+                Header = "Type",
+                Width = new DataGridLength(1, DataGridLengthUnitType.Star), // for some reason the constructor has value and unit type but value doenst do anything if its star
+                Binding = new Binding("Type")
+            };
+
+            TableGrid.Columns.Add(id_col);
+            TableGrid.Columns.Add(room_id_col);
+            TableGrid.Columns.Add(type_col);
+        }
+
+        private void roomsBtn_Click(object sender, RoutedEventArgs e)
+        {
+            TableGrid.Columns.Clear();
+
+            TableGrid.ItemsSource = RoomsList;
+
+            DataGridTextColumn id_col = new DataGridTextColumn()
+            {
+                Header = "ID",
+                Binding = new Binding("Id")
+            };
+
+            DataGridTextColumn floor_col = new DataGridTextColumn()
+            {
+                Header = "Floor",
+                Binding = new Binding("Floor")
+            };
+
+            DataGridTextColumn roomnb_col = new DataGridTextColumn()
+            {
+                Header = "RoomNB",
+                Binding = new Binding("RoomNb")
+            };
+
+            DataGridTextColumn occupancy_col = new DataGridTextColumn()
+            {
+                Header = "Occupancy",
+                Binding = new Binding("Occupancy")
+            };
+
+            DataGridTextColumn type_col = new DataGridTextColumn()
+            {
+                Header = "Type",
+                Width = new DataGridLength(1, DataGridLengthUnitType.Star),
+                Binding = new Binding("Type")
+            };
+
+            TableGrid.Columns.Add(id_col);
+            TableGrid.Columns.Add(floor_col);
+            TableGrid.Columns.Add(roomnb_col);
+            TableGrid.Columns.Add(occupancy_col);
+            TableGrid.Columns.Add(type_col);
+        }
+    }
+}
