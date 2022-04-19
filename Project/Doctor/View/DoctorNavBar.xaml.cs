8257c5fad22899a845930d66e0015c94f43cf2d6
Jelena Dinić <jelenaad26@gmail.com>
2022-04-19 10:19:50 +0200

    new classes in model added

---
+++ Project/Doctor/View/DoctorNavBar.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Doctor.View
+{
+    /// <summary>
+    /// Interaction logic for DoctorNavBar.xaml
+    /// </summary>
+    public partial class DoctorNavBar : Window
+    {
+        public DoctorNavBar()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonSchedule(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ButtonExaminations(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ButtonRequests(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ButtonPatients(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ButtonNotifications(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ButtonLogOut(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+
+    }
+}
