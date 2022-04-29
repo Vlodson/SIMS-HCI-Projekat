f6452c2527bbb2be2f390ade9f37cbe653b28e2a
Sandra Brkic <sandra.brkic5@gmail.com>
2022-04-29 13:11:56 +0200

    Remove excessive code

--- Project/HospitalMain/Repository/MedicalRecordRepo.cs
+++ Project/HospitalMain/Repository/MedicalRecordRepo.cs
@@ -35,43 +35,43 @@ namespace Repository
             ObservableCollection<Report> reports = new ObservableCollection<Report>();
             reports.Add(report);
 
-            //String allergenName1 = "Prasina";
-            //String allergenName2 = "Polen trave";
-            //String allergenName3 = "Grinje";
-            //String allergenName4 = "Lesnici";
+            String allergenName1 = "Prasina";
+            String allergenName2 = "Polen trave";
+            String allergenName3 = "Grinje";
+            String allergenName4 = "Lesnici";
 
-            //Allergen allergen1 = new Allergen(allergenName1);
-            //Allergen allergen2 = new Allergen(allergenName2);
-            //Allergen allergen3 = new Allergen(allergenName3);
-            //Allergen allergen4 = new Allergen(allergenName4);
+            Allergen allergen1 = new Allergen(allergenName1);
+            Allergen allergen2 = new Allergen(allergenName2);
+            Allergen allergen3 = new Allergen(allergenName3);
+            Allergen allergen4 = new Allergen(allergenName4);
 
-            //ObservableCollection<Allergen> allergens1 = new ObservableCollection<Allergen>();
-            //ObservableCollection<Allergen> allergens2 = new ObservableCollection<Allergen>();
-            //ObservableCollection<Allergen> allergens3 = new ObservableCollection<Allergen>();
+            ObservableCollection<Allergen> allergens1 = new ObservableCollection<Allergen>();
+            ObservableCollection<Allergen> allergens2 = new ObservableCollection<Allergen>();
+            ObservableCollection<Allergen> allergens3 = new ObservableCollection<Allergen>();
 
-            //allergens1.Add(allergen1);
-            //allergens1.Add(allergen2);
+            allergens1.Add(allergen1);
+            allergens1.Add(allergen2);
 
-            //allergens2.Add(allergen3);
+            allergens2.Add(allergen3);
 
-            //allergens3.Add(allergen4);
-            //allergens3.Add(allergen1);
+            allergens3.Add(allergen4);
+            allergens3.Add(allergen1);
 
-            //List<Examination> examinationsDoctor1 = new List<Examination>();
-            //DateTime dtDoctor1 = DateTime.Now;
-            //Doctor doctor1 = new Doctor("d1", "Ivan", "Peric", dtDoctor1, DoctorType.Pulmonology, examinationsDoctor1);
+            List<Examination> examinationsDoctor1 = new List<Examination>();
+            DateTime dtDoctor1 = DateTime.Now;
+            Doctor doctor1 = new Doctor("d1", "Ivan", "Peric", dtDoctor1, DoctorType.Pulmonology, examinationsDoctor1);
 
-            //List<Examination> examinationsDoctor2 = new List<Examination>();
-            //DateTime dtDoctor2 = DateTime.Now;
-            //Doctor doctor2 = new Doctor("d11", "Marko", "Markovic", dtDoctor2, DoctorType.Pulmonology, examinationsDoctor2);
+            List<Examination> examinationsDoctor2 = new List<Examination>();
+            DateTime dtDoctor2 = DateTime.Now;
+            Doctor doctor2 = new Doctor("d11", "Marko", "Markovic", dtDoctor2, DoctorType.Pulmonology, examinationsDoctor2);
 
-            //MedicalRecord mr1 = new MedicalRecord("1", "0605994802463", "Pera", "Peric", "4098240", "pera@mail.com", "Partizanska 13, Novi Sad", Gender.Male, new DateTime(1994, 05, 06), "A", reports, allergens1);
-            //MedicalRecord mr2 = new MedicalRecord("2", "0808985802463", "Ivan", "Ivic", "4489965", "ivan@mail.com", "Partizanska 14, Novi Sad", Gender.Male, new DateTime(1985, 08, 08), "B", reports, allergens2);
-            //MedicalRecord mr3 = new MedicalRecord("3", "1111001802463", "Zika", "Zikic", "41478115", "zika@mail.com", "Partizanska 15, Novi Sad", Gender.Male, new DateTime(2001, 11, 11), "AB", reports, allergens3);
+            MedicalRecord mr1 = new MedicalRecord("1", "0605994802463", "Pera", "Peric", "4098240", "pera@mail.com", "Partizanska 13, Novi Sad", Gender.Male, new DateTime(1994, 05, 06), "A", reports, allergens1);
+            MedicalRecord mr2 = new MedicalRecord("2", "0808985802463", "Ivan", "Ivic", "4489965", "ivan@mail.com", "Partizanska 14, Novi Sad", Gender.Male, new DateTime(1985, 08, 08), "B", reports, allergens2);
+            MedicalRecord mr3 = new MedicalRecord("3", "1111001802463", "Zika", "Zikic", "41478115", "zika@mail.com", "Partizanska 15, Novi Sad", Gender.Male, new DateTime(2001, 11, 11), "AB", reports, allergens3);
 
-            //this.MedicalRecords.Add(mr1);
-            //this.MedicalRecords.Add(mr2);
-            //this.MedicalRecords.Add(mr3);
+            this.MedicalRecords.Add(mr1);
+            this.MedicalRecords.Add(mr2);
+            this.MedicalRecords.Add(mr3);
 
 
             //if (File.Exists(dbPath))
