5fee31bc4402e7cdd477f31c9a6901f76ff4a1e3
Vladimir Lunić <vladimir.lunic2000@gmail.com>
2022-05-26 08:14:39 +0200

    MVVM choose form window

---
+++ Project/Admin/ViewModel/ChooseFormViewModel.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+using Controller;
+using Utility;
+
+namespace Admin.ViewModel
+{
+    public class ChooseFormViewModel
+    {
+        public ICommandTemplate<String> NavigationCommand { get; private set; }
+
+        private readonly RoomController roomController;
+        public String Title;
+
+        public ChooseFormViewModel()
+        {
+            NavigationCommand = new ICommandTemplate<String>(OnNavigation);
+
+            var app = Application.Current as App;
+            roomController = app.roomController;
+
+            Title = "Choose form for room\n" + roomController.GetClipboardRoom().RoomNb;
+        }
+
+        public void OnNavigation(String view)
+        {
+            switch (view)
+            {
+                case "back":
+                    break;
+                case "help":
+                    break;
+                case "logout":
+                    break;
+                case "equipmentTransfer":
+                    break;
+                case "changeType":
+                    break;
+                case "renovation":
+                    break;
+            }
+        }
+    }
+}
