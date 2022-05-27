1813694f0a4341865278e9d3ff49b32e8e690824
Vladimir Lunić <vladimir.lunic2000@gmail.com>
2022-05-27 20:03:32 +0200

    Add commands to VM for equipment

--- Project/Admin/ViewModel/EquipmentTableViewModel.cs
+++ Project/Admin/ViewModel/EquipmentTableViewModel.cs
@@ -10,14 +10,32 @@ using Controller;
 using Utility;
 using HospitalMain.Enums;
 
+using Admin.Views;
+
 namespace Admin.ViewModel
 {
     public class EquipmentTableViewModel: BindableBase
     {
+        public ICommandTemplate<String> NavigationCommand { get; private set; }
+        public ICommandTemplate RemoveCommand { get; private set; }
+        public ICommandTemplate QueryCommand { get; private set; }
+
         private EquipmentController equipmentController;
+        private MainWindow mainWindow = Application.Current.MainWindow as MainWindow;
 
-
-
+        private String search;
+        public String Search
+        {
+            get { return search; }
+            set
+            {
+                if (search != value)
+                {
+                    search = value;
+                    OnPropertyChanged("Search");
+                }
+            }
+        }
 
         private ObservableCollection<FriendlyEquipment> equipment;
         public ObservableCollection<FriendlyEquipment> Equipment
@@ -39,11 +57,15 @@ namespace Admin.ViewModel
                 {
                     selectedEquipment = value;
                     OnPropertyChanged("SelectedEquipment");
+                    RemoveCommand.RaiseCanExecuteChanged();
                 }
             }
         }
         public EquipmentTableViewModel()
         {
+            NavigationCommand = new ICommandTemplate<String>(OnNavigation);
+            RemoveCommand = new ICommandTemplate(OnRemove, CanRemove);
+            QueryCommand = new ICommandTemplate(OnQuery);
 
             var app = Application.Current as App;
             equipmentController = app.equipmentController;
@@ -54,22 +76,64 @@ namespace Admin.ViewModel
             Equipment = new ObservableCollection<FriendlyEquipment>();
             foreach(Equipment equipmentItem in equipment)
                 Equipment.Add(new FriendlyEquipment(equipmentItem));
+
+            Search = "Enter Query";
         }
 
-        public void RemoveEquipment()
+        public void OnRemove()
         {
             equipmentController.RemoveEquipment(SelectedEquipment.Id, SelectedEquipment.RoomId);
             Equipment.Remove(SelectedEquipment);
         }
-
-        public void QueryEquipment(String query)
+        public bool CanRemove()
         {
-            ObservableCollection<Equipment> queriedEquipment = equipmentController.QueryEquipment(query);
-            Equipment = new ObservableCollection<FriendlyEquipment>();
+            return SelectedEquipment is not null;
+        }
+
+        public void OnQuery()
+        {
+            if (String.IsNullOrEmpty(Search))
+            {
+                ObservableCollection<Equipment> equipment = equipmentController.ReadAll();
+                Equipment.Clear();
+                foreach (Equipment equipmentItem in equipment)
+                    Equipment.Add(new FriendlyEquipment(equipmentItem));
+                return;
+            }
+
+            ObservableCollection<Equipment> queriedEquipment = equipmentController.QueryEquipment(Search);
+            Equipment.Clear();
             foreach(Equipment queriedItem in queriedEquipment)
                 Equipment.Add(new FriendlyEquipment(queriedItem));
             
         }
+
+        public void OnNavigation(String view)
+        {
+            switch (view)
+            {
+                case "back":
+                    mainWindow.Width = 750;
+                    mainWindow.Height = 430;
+                    mainWindow.CurrentView = new MainMenuView();
+                    break;
+                case "logout":
+                    break;
+                case "rooms":
+                    mainWindow.CurrentView = new RoomTableView();
+                    break;
+                case "medicine":
+                    mainWindow.CurrentView = new MedicineTableView();
+                    break;
+                case "transfers":
+                    mainWindow.CurrentView = new EquipmentTransferTableView();
+                    break;
+                case "renovations":
+                    mainWindow.CurrentView = new RenovationTableView();
+                    break;
+            }
+        }
+
     }
 
     public class FriendlyEquipment
